226252fd6ec9d2402a654a3087d2876a40fcd078
Isabella <chou.isabella@gmail.com>
2026-02-28 10:55:07 -0600

    finish A8

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,5 +9,7 @@ data Calc a = Add a
 
 
 calc :: Num a => [Calc a] -> a -> (a -> a) -> (a -> a) -> a
-calc = undefined
-
+calc xx init ka ks = aux xx ka ks where
+    aux [] ka' ks' = ks' (ka' init)
+    aux (Add i : rest) ka' ks' = aux rest (ka' . (+ i)) ks'
+    aux (Sub i : rest) ka' ks' = aux rest ka' (ks' . (subtract i))
